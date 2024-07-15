850395a148f58c8de3e85f12cdf94b830fe3b1d7
Josiepurba <josepurba2001@gmail.com>
2024-07-15 21:07:27 +0700

    first commit

---
+++ src/components/Experience/index.js
@@ -0,0 +1,141 @@
+
+import React from 'react'
+import styled from 'styled-components'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import ExperienceCard from '../Cards/ExperienceCard';
+import { experiences } from '../../data/constants';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    z-index: 1;
+    align-items: center;
+    padding: 40px 0px 80px 0px;
+    @media (max-width: 960px) {
+        padding: 0px;
+    }
+`;
+
+const Wrapper = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1350px;
+    padding: 80px 0;
+    gap: 12px;
+    @media (max-width: 960px) {
+        flex-direction: column;
+    }
+`;
+
+const Title = styled.div`
+font-size: 42px;
+text-align: center;
+font-weight: 600;
+margin-top: 20px;
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 768px) {
+      margin-top: 12px;
+      font-size: 32px;
+  }
+`;
+
+const Desc = styled.div`
+    font-size: 18px;
+    text-align: center;
+    max-width: 600px;
+    color: ${({ theme }) => theme.text_secondary};
+    @media (max-width: 768px) {
+        margin-top: 12px;
+        font-size: 16px;
+    }
+`;
+
+const TimelineSection = styled.div`
+    width: 100%;
+    max-width: 1000px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`;
+
+export const CertificateButton = styled.a`
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+    text-decoration: none;
+    width: 50%;
+    max-width: 100px;
+    text-align: center;
+    padding: 16px 0;
+    color:${({ theme }) => theme.white};
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 10px;
+    font-weight: 400;
+    transition: all 0.2s ease-in-out !important;
+    background: hsla(271, 100%, 50%, 1);
+    background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    box-shadow:  20px 20px 60px #1F2634,
+    -20px -20px 60px #1F2634;
+    &:hover {
+        transform: scale(1.05);
+    transition: all 0.4s ease-in-out;
+    box-shadow:  20px 20px 60px #1F2634,
+    filter: brightness(1);
+    }    
+    
+    
+    @media (max-width: 400px) {
+        padding: 12px 0;
+        font-size: 18px;
+    } 
+`;
+
+
+const index = () => {
+    return (
+        <Container id="experience">
+            <Wrapper>
+                <Title>Experience</Title>
+                <Desc>
+                    I actually don't have any work experience, only that I have attended internships and bootcamps.
+                </Desc>
+                <TimelineSection>
+                    <Timeline>
+                        {experiences.map((experience,index) => (
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot variant="outlined" color="secondary" />
+                                    {index !== experiences.length - 1 && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                </TimelineSeparator>
+                                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                    <ExperienceCard experience={experience}/>
+                                    <CertificateButton href={experience.certificate} target='display'>Check certificate</CertificateButton>
+                                </TimelineContent>
+                            </TimelineItem>
+                        ))}
+                    </Timeline>
+
+                </TimelineSection>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default index
---
+++ src/data/constants.js
@@ -0,0 +1,173 @@
+
+export const Bio = {
+  name: "Josie G.F. Purba",
+  roles: [
+    "Front-End Developer"
+  
+  ],
+  description:
+    "I am a motivated and outgoing individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high quality results. With a positive attitude and growth mindset, I am ready to make meaningful contributions and achieve great things.",
+  github: "https://github.com/Josiepurba",
+  resume:
+    "https://drive.google.com/file/d/1rqnERcTY8Pjk_M5hHYLA5KtOwEYJ1F9e/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/josie-g-f-purba/",
+  insta: "https://www.instagram.com/josiepurba/",
+  facebook: "https://web.facebook.com/sidagambir93/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://avatars.githubusercontent.com/u/18133?s=280&v=4",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://backend.teltics.in/img_blog/artikel_i.c.t/2024/06/24062511530512464.jpg",
+      },
+      {
+        name: "Figma",
+        image :
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx28mg8aQ39CEGURVztotd5VC0bO7Ik-ZpBw&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://karirlab-prod-bucket.s3.ap-southeast-1.amazonaws.com/files/privates/p4GzqSJc0MDdb56pw6RSvF6gmKASzUCOG3SPz5ik.png",
+    role: "Fullstack Web Developer Intern",
+    company: "Binary Academy",
+    date: "Feb 2022 - Aug 2022",
+    desc: "Learn more deeply about the structure of the web starting from the Frontend, API, and Backend,Create a Final Project in the form of e-commerce used goods Learn Communication Skills, Management Skills, and Leadership Skills.",
+    skills: [
+      "ReactJS",
+      "JavaScript",
+      "NodeJs",
+      "Postman",
+      "HTML",
+      "CSS",
+    ],
+    certificate :"https://drive.google.com/file/d/1hmMtBkC40vyWnVAJkAjrFTKK2xkIYgbE/view?usp=drive_link"
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://a-cdn.sindonews.net/dyn/620/content/2015/03/16/144/977177/manajemen-unsri-prodi-favorit-snmptn-x8C-thumb.jpg",
+    school: "Universitas Sriwijaya",
+    date: "Aug 2019 - Maret 2024",
+    grade: "3.40 GPA",
+    desc: "I graduated from UNSRI majoring in informatics engineering with a study period of 4 years 8 months and a GPA of 3.40",
+    degree: "Fresh Graduate of Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://yt3.googleusercontent.com/75ek4M4M4lAo3K8kl7kIPR_QfeQ4zyyqR5kxU3fQskb1VWr828nKWz8NNOtFcvfkAfH73XQTfg=s900-c-k-c0x00ffffff-no-rj",
+    school: "SMA N 1 RAYA",
+    date: "Apr 2016 - Mei 2019",
+    grade: "88%",
+    desc: "High school at SMA N 1 Raya and I majored in mathematics with an average score of 88 and after graduating I continued at UNSRI.",
+    degree: "Mathematics and natural science",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Classification of nutritional status of toddlers",
+    date: "Apr 2023 - Des 2023",
+    description:
+      "This is a machine learning project using the K-Nearest Neighbor method to classify the nutritional status of babies into 3 groups, namely over-nutrition, good nutrition and under-nutrition..",
+    image:"https://raw.githubusercontent.com/Josiepurba/Klasifikasi-Status-Gizi-Balita-Dengan-KNN/main/Hasil.png",
+    tags: ["Python", "Keras"],
+    github: "https://github.com/Josiepurba/Klasifikasi-Status-Gizi-Balita-Dengan-KNN",
+    webapp: "https://brain-tumor.netlify.app/",
+
+  },
+  {
+    id: 1,
+    title: "Second Hand",
+    date: "Jul 2022 - Aug 2022",
+    description:
+      "the creation of used goods e-commerce which provides all used goods from kitchen utensils to home equipment that are still suitable for use.",
+    image:
+      "https://raw.githubusercontent.com/Josiepurba/SecondHand/main/FE-Team-1-Final-Project-master/SecondHand-1.png ",
+    tags: ["Javasript", "React.js", "Node.js",],
+    github: "https://github.com/Josiepurba/SecondHand",
+
+  }
+];
+
